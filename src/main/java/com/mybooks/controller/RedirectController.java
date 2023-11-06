f30c8efb3a8aac821fc9509c0c020b88a018f5e0
Bobby Iliev <104756790+Bobogamen@users.noreply.github.com>
2023-11-06 11:45:32 +0200

    Update RedirectController.java
    Redirect URL change

--- src/main/java/com/mybooks/controller/RedirectController.java
+++ src/main/java/com/mybooks/controller/RedirectController.java
@@ -8,7 +8,7 @@ public class RedirectController {
 
     @RequestMapping("/**")  // Handle all requests
     public String redirectToTarget() {
-        String redirectUrl = "https://mybooks.up.railway.app";
+        String redirectUrl = "https://mybooksbg.netlify.app";
         return "redirect:" + redirectUrl;
     }
 }
